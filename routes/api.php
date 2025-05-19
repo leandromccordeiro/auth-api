2345fe738400618d2802fc1ad4ae8ea2782ed1cf
leandromccordeiro <leandroo_marcos@yahoo.com.br>
2025-05-19 17:07:53 -0300

    Organiza rotas de autenticação em um grupo prefixado no arquivo api.php

--- routes/api.php
+++ routes/api.php
@@ -7,10 +7,11 @@ use App\Http\Controllers\UserController;
 
 // Route::get('/teste', [UserController::class, 'teste']);
 // Route::post('/envio', [UserController::class, 'envio']);
-
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
-Route::post('/logout', [AuthController::class, 'logout']);
+Route::prefix('auth')->group(function () {
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
 
 Route::get('/user', function (Request $request) {
     return $request->user();
